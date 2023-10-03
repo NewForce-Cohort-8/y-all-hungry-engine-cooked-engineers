ff26683606c188c8d56ceab6192b1c99af17b0a6
Hannah Thompson <hannahmthompsonedu@gmail.com>
2023-10-03 11:07:10 -0400

    updated setter & render for ice cream drop down

--- scripts/IceCream.js
+++ scripts/IceCream.js
@@ -2,13 +2,6 @@ import { getIceCream, setIceCream } from "./dataAccess.js";
 
 const IceCream = getIceCream();
 
-const displayIceCream = () => {
-	return IceCream.map((iceCream) => {
-			return `<option value="${iceCream.id} id="location--${iceCream.id}" class="option dropdown">${iceCream.name}</option>`;
-		})
-		.join("");
-};
-
 export const IceCreams = () => {
 	return `<section id="iceCream--section">
         <label for="iceCream">Select an ice cream for your order</label>
