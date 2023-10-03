7f05485c65f71581c1341d51d35984bd73895434
Hunter Owens <85654096+HunterO50@users.noreply.github.com>
2023-10-03 09:32:05 -0400

    Toys database in progress

--- scripts/database.js
+++ scripts/database.js
@@ -1,6 +1,18 @@
 const database = {
-    transientState: {}
+    transientState: {},
+
+    toys : [
+        {id: 1, name:"Moon Shoes", img:"", price:10, desc:"Moon Shoes! They make you jump like you're on the moon!"},
+    {id:2, name:"Gas-Powered Pogo Stick", img:"", price: 75, desc:"Ankle snapping pogo power!"},
+    {id:3, name:"Portable Ouija Board", img:"", price:2, desc:"For consulting demons on the go!" },
+    {id:4, name:"Box O'Rocks", img:"", price:5, desc:"Definitely not from the parking lot!"},
+    {id:5, name:"Hot Dawg Launcher", img:"", price:35, desc:"For eating Dawgs at a distance!"}
+]
 }
+export const getToys = () => {
+    return database.getToys.map(f => ({...f}))
+}
+
 
 export const setLocation = (locationId) => {
     database.transientState.selectedLocation = locationId
@@ -8,7 +20,7 @@ export const setLocation = (locationId) => {
 }
 
 export const getLocations = () => {
-    return database.getLocations.map(f => ({...f}))
+    return database.getLocations.map(toy => ({...toy}))
 }
 
 export const completeOrder = () => {
