cca733de726473e2441fceff9d0bb1cca6983000
Colin Matteson <seefm@proton.me>
2023-10-03 09:31:14 -0400

    first outlines of drinks database buildout

--- scripts/database.js
+++ scripts/database.js
@@ -1,4 +1,40 @@
 const database = {
+    drinks: [
+        { 
+            id: 1,
+            name: "Coke",
+            img: "",
+            price: 1.99
+        },
+
+        {
+            id: 2,
+            name: "Diet Coke",
+            img: "",
+            price: 1.99
+        },
+
+        {
+            id: 3,
+            name: "Mountin Dew",
+            img: "",
+            price: 1.99
+        },
+
+        {
+            id: 4,
+            name: "Bottled Water",
+            img: "",
+            price: 2.99
+        },
+
+        {
+            id: 5,
+            name: "Hot Dog Water (We'll Pour It Directly In Your Mouth)",
+            img: "",
+            price: 3.99
+        }
+],
     transientState: {}
 }
 
